b2a207f0d4b79e4c58f41ea59fbb77744617c37f
0racles <oracleslonge@gmail.com>
2016-06-22 18:38:20 +0200

    caching
    ...more img files

--- html/sw.js
+++ html/sw.js
@@ -2,10 +2,17 @@
 this.addEventListener("install", function (event) {
       event.waitUntil(
         caches.open('v1').then(function (cache) {
-          cache.addAll(['/html/', '/html/img/trapped_lady.jpg', '/html/img/screaming.jpg', '/html/img/location4.jpg', '/html/img/embrace3.jpg']);
+          cache.addAll(['/html/', 
+          	'/html/img/trapped_lady.jpg', 
+          	'/html/img/screaming.jpg', 
+          	'/html/img/location4.jpg', 
+          	'/html/img/embrace3.jpg',
+          	'/html/img/bg.jpg',
+          	'/html/img/timer.png'
+          	]);
         })
         );
-    });
+    })
 
 this.addEventListener("fetch", function (event) {
 	event.respondWith(caches.match(event.request))
