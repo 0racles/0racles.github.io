c6fe512f561da62dcb24c830024005a514494922
0racles <oracleslonge@gmail.com>
2016-06-22 14:23:45 +0200

    update
    changing the sw algo

--- html/js/911.js
+++ html/js/911.js
@@ -45,7 +45,7 @@ chunks = [],
 
 initiate_sw =function () {
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/html/sw.js', {scope : '/html/'}).then(function (reg) {
+  navigator.serviceWorkerContainer.register('/html/sw.js', {scope : '/html/'}).then(function (reg) {
     console.log("you have succesfully registered. Scope is " + reg.scope);
   }).catch(function(error) { 
     console.log('Registration failed with ' + error);
