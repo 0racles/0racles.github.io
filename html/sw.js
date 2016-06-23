86045ce9bc7c8f72eb29bbaf74b5c1e3b9b76d4a
0racles <oracleslonge@gmail.com>
2016-06-23 16:47:01 +0200

    debug...

--- html/sw.js
+++ html/sw.js
@@ -14,7 +14,7 @@ this.addEventListener("install", function (event) {
         );
     })
 
-this.addEventListener("fetch", function (event) {
+/*this.addEventListener("fetch", function (event) {
 	event.respondWith(
     caches.match(event.request));
-});    
+});*/    
