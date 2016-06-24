d7b805e77f7d182838d53008207b088cbf673607
0racles <oracleslonge@gmail.com>
2016-06-24 13:03:49 +0200

    test
    trying out another respond style

--- html/s-w.js
+++ html/s-w.js
@@ -2,11 +2,23 @@
 this.addEventListener("install", function (event) {
       event.waitUntil(
         caches.open('v2').then(function (cache) {
-         return cache.addAll(['/html/', '/html/index.html', '/html/js/', '/html/js/911.js', '/html/img/', '/html/img/trapped_lady.jpg', '/html/img/screaming.jpg', '/html/img/location4.jpg', '/html/img/embrace3.jpg']);
+         return cache.addAll(['/html/', 
+          '/html/index.html', 
+          '/html/js/', 
+          '/html/js/911.js', 
+          '/html/img/', 
+          '/html/img/trapped_lady.jpg', 
+          '/html/img/screaming.jpg', 
+          '/html/img/location4.jpg', 
+          '/html/img/embrace3.jpg'
+          ]);
         })
         );
     });
 
 this.addEventListener('fetch', function (event) {
-	event.respondWith(caches.match(event.request));
+	event.respondWith(
+    //caches.match(event.request)
+    new Response(alert('iwill never out-grow the thirill of xmas day'))
+    );
 });
