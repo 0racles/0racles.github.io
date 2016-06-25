be1ce36ebe57c71532d1de1d90affcb1ece4484f
0racles <oracleslonge@gmail.com>
2016-06-25 16:35:30 +0200

    new change
    making somechanges to get the new sw up  and running

---
+++ html/js/serviceworker.js
@@ -0,0 +1,21 @@
+// it all goes here
+this.addEventListener("install", function (event) {
+      event.waitUntil(
+        caches.open('v3').then(function (cache) {
+         return cache.addAll(['/html/', 
+          '/html/img/trapped_lady.jpg', 
+          '/html/img/screaming.jpg', 
+          '/html/img/location4.jpg', 
+          '/html/img/embrace3.jpg'
+          ]);
+        })
+        );
+    });
+
+
+
+this.addEventListener('fetch', function (event) {
+	event.respondWith(
+    caches.match(event.request)
+    );
+});
