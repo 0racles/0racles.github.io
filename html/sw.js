c3c596f3db1f01cb57359f4a41d6dcd1ef604eed
0racles <oracleslonge@gmail.com>
2016-06-27 00:17:22 +0200

    v3
    adding version 3 with all important scripts

--- html/sw.js
+++ html/sw.js
@@ -34,6 +34,41 @@ this.addEventListener("install", function (event) {
         );
     });
 
+this.addEventListener("install", function (event) {
+      event.waitUntil(
+        caches.open('v3').then(function (cache) {
+         return cache.addAll(['/html/', 
+          '/html/index.html',
+          '/html/js/911.js', 
+          '/html/js/all_scr.js',
+          '/html/js/app.js',
+          '/html/js/bootstrap.min.js',
+          '/html/js/emergency.js',
+          '/html/js/jquery.min.js',
+          '/html/js/jquery.parallax-0.2-min.js',
+          '/html/css/adaptive.css',
+          '/html/css/animate.css',
+          '/html/css/bootstrap.css',
+          '/html/css/main.css',
+          '/html/css/pushy.css',
+          '/html/css/font-awesome.css',
+          '/html/img/trapped_lady.jpg', 
+          '/html/img/screaming.jpg', 
+          '/html/img/location4.jpg', 
+          '/html/img/embrace3.jpg,'
+          '/html/img/start/bg.jpg',
+          '/html/fontawesome-webfont.eot',
+          '/html/fontawesome-webfont.svg',
+          '/html/fontawesome-webfont.ttf',
+          '/html/fontawesome-webfont.woff',
+          '/html/fontawesome-webfront.woff2',
+          '/html/FontAwesome-webfront.otf,'
+          ]);
+        })
+        );
+    });
+
+
 /*this.addEventListener('activate', function(event) {
   var cacheWhitelist = ['v1'];
 
