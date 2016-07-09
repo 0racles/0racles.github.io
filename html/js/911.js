c2ff1583b2a1e3c095771e43a672b4b37c9c4014
0racles <oracleslonge@gmail.com>
2016-07-09 09:00:32 +0200

    vidualize
    adding visualization of voice input

--- html/js/911.js
+++ html/js/911.js
@@ -43,6 +43,8 @@ span_3,
 existing_data = {},
 storedData,
 chunks = [],
+audioCtx = new (window.AudioContext || webkitAudioContext)(),
+canvasCtx = canvas.getContext("2d"),
 // dropdown for microphone settings
 
 initiate_sw =function () {
@@ -301,6 +303,7 @@ asr = function () {
   recognition.onresult = function (event) {
     var password = event.results[0][0].transcript;
     console.log(password);
+    visualize(stream);
     msg = new SpeechSynthesisUtterance(),
     voices = window.speechSynthesis.getVoices();
       msg.voice = voices[8];
@@ -332,6 +335,60 @@ recognition.onerror = function () {
   console.log('An error was found');
 }
 
+},
+
+visualize = function (stream) {
+  var source = audioCtx.createMediaStreamSource(stream);
+
+  var analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+  //analyser.connect(audioCtx.destination);
+  
+  WIDTH = canvas.width
+  HEIGHT = canvas.height;
+
+  draw()
+
+  function draw() {
+
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+
+
+    for(var i = 0; i < bufferLength; i++) {
+ 
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+
+  }
 },
 
 invite_contact_func = function () {
