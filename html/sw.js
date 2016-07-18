26638e93175725f424bba84586d4ac2333e7edd5
0racles <oracleslonge@gmail.com>
2016-07-18 07:40:19 +0200

    version 25
    updating to v25

--- html/sw.js
+++ html/sw.js
@@ -1,9 +1,9 @@
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v24').then(function (cache) {
+        caches.open('v25').then(function (cache) {
          return cache.addAll(['/html/', 
           '/html/index.html',
-          '/html/manifest.json',
+          '/manifest.json',
           '/html/emergency.html',
           '/html/js/911.js', 
           '/html/js/all_scr.js',
@@ -44,7 +44,7 @@ this.addEventListener("install", function (event) {
     });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v24'];
+  var cacheWhitelist = ['v25'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
