e1c2361e7b7c91a8b29bb5ec47ce95392bca5da6
0racles <oracleslonge@gmail.com>
2016-07-23 12:58:09 +0200

    a
    b

--- html/js/911.js
+++ html/js/911.js
@@ -75,7 +75,7 @@ event.waitUntil(() =>
 initiate_sw = function () {
   //var source = new EventSource("demo_sse.php");
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/html/s-w.js', {scope : '/html/'}).then(function (reg) {
+  navigator.serviceWorker.register('/html/sw.js', {scope : '/html/'}).then(function (reg) {
     console.log("you have succesfully registered. Scope is " + reg.scope);
     reg.pushManager.subscribe().then(function (pushSubscription) {
       console.log(pushSubscription.endpoint);
--- html/sw.js
+++ html/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v3').then(function (cache) {
+        caches.open('v4').then(function (cache) {
          return cache.addAll(['/html/', 
           '/html/index.html',
           '/html/manifest.json',
@@ -46,7 +46,7 @@ this.addEventListener("install", function (event) {
     });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v3'];
+  var cacheWhitelist = ['v4'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
