4d0de85340abc70cfc2d10c1a89f37903c2807d4
0racles <oracleslonge@gmail.com>
2016-08-03 17:57:27 +0200

    css
    styling not. ui

--- html/sw.js
+++ html/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v18').then(function (cache) {
+        caches.open('v19').then(function (cache) {
          return cache.addAll(['/html/', 
           '/html/index.html',
           '/html/manifest.json',
@@ -55,7 +55,7 @@ this.addEventListener("install", function (event) {
     });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v18'];
+  var cacheWhitelist = ['v19'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
