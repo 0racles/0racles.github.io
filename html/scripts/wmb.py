316eb98967c722c7d2c96e94127a2b2cf96ada68
0racles <oracleslonge@gmail.com>
2016-09-02 19:41:17 +0200

    v16
    Testing wmb.txt

--- html/scripts/wmb.py
+++ html/scripts/wmb.py
@@ -1,9 +1,14 @@
-with open("https://0racles.githib.io/html/index.html", "r") as my_file:
+""" with open("https://0racles.githib.io/html/index.html", "r") as my_file:
 	print my_file.readline()
 	if my_file.closed:
 		my_file.close()
 
 with open ("https://0racles.github.io/html/scripts/wmb.txt", "w") as wmb_file:
 	wmb_file.write(my_file)
+	if wmb_file.closed:
+		wmb_file.close() """
+
+with open("https://0racles.github.io/html/scripts/wmb.txt", "w") as wmb_file:
+	wmb_file.write("Hello World")
 	if wmb_file.closed:
 		wmb_file.close()
