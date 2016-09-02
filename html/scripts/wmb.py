4b0eacef7db31f5c134731da5fc80da476980dd5
0racles <oracleslonge@gmail.com>
2016-09-02 20:05:53 +0200

    v18
    updating v18

--- html/scripts/wmb.py
+++ html/scripts/wmb.py
@@ -1,3 +1,3 @@
-with open("https://0racles.github.io/html/scripts/wmb.txt", "w") as wmb_file:
-	wmb_file.write("Hello World!")
-	
+wmb_file = open ("https://0racles.github.io/html/scripts/wmb.txt", "w")
+wmb_file.write("Hello World!")
+wmb_file.close()
