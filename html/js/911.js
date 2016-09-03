3705cd65bea0a3441fe21ea4e6a05b517ce9a549
0racles <oracleslonge@gmail.com>
2016-09-03 11:02:22 +0200

    v19
    correcting error in 911.js

--- html/js/911.js
+++ html/js/911.js
@@ -84,7 +84,7 @@ if ('serviceWorker' in navigator) {
           navigator.serviceWorker.ready.then(function(reg) {
             if (!sub) {
               reg.pushManager.subscribe({
-                userVisibleOnly : true
+                userVisibleOnly : true,
                 applicationServerKey : 'AIzaSyDlGjhrU1idWDLs_IUhu1dc2xh-Z_Kvvto'
               })
               return sub;
