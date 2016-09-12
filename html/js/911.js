c0a98703128e5e628cce04e8d25e3fef6ce34e1b
0racles <oracleslonge@gmail.com>
2016-09-12 17:33:15 +0200

    911.js
    testing another console log

--- html/js/911.js
+++ html/js/911.js
@@ -92,7 +92,7 @@ if ('serviceWorker' in navigator) {
           }).then(function(sub) { 
             console.log('endpoint: ' + sub.endpoint)
             console.log(JSON.stringify(sub))
-            console.log("i am now on my own")
+            console.log("This is my fathers world")
             var fetchOptions = {
               method : 'POST',
               headers : new Headers({'Content-Type' : 'application/json'}),
