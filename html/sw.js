65e19bae7c5a40c7caa0d553eaa098505b5978e9
0racles <Dare.Longe@city.ac.uk>
2016-11-17 13:08:31 +0000

    sw18
    debugging ui to fit image to full screen height

--- html/sw.js
+++ html/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v17').then(function (cache) {
+        caches.open('v18').then(function (cache) {
          return cache.addAll(['/html/', 
           '/html/index.html',
           '/html/manifest.json',
@@ -56,7 +56,7 @@ this.addEventListener("install", function (event) {
     });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v17'];
+  var cacheWhitelist = ['v18'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
