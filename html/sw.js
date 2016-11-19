e875fafa422e3a3478f6d11c404cc7327ca19e2f
0racles <Dare.Longe@city.ac.uk>
2016-11-19 16:44:00 +0000

    sw v23
    debugging fa-icons at the emerhgency page and adjusting avatar position.
    discovered that the fa icons wasn't  showing because the link tags in
    the html markup was wrongly spaced

--- html/sw.js
+++ html/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('v22').then(function (cache) {
+        caches.open('v223').then(function (cache) {
          return cache.addAll(['/html/', 
           '/html/index.html',
           '/html/manifest.json',
@@ -56,7 +56,7 @@ this.addEventListener("install", function (event) {
     });
 
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v22'];
+  var cacheWhitelist = ['v23'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
